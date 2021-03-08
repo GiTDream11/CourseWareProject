821076f766468509c585e73575d5cc6dd45364c6
JarodAlgra <43503965+JarodAlgra@users.noreply.github.com>
2021-03-07 19:17:36 -0800

    Jarod/more html (#2)
    * initial commit
    
    * fleshing out html, adding accordion for topics

---
+++ scripts/index.js
@@ -0,0 +1,15 @@
+// const topics = document.querySelectorAll(".topic");
+
+// for (let i = 0; i < topics.length; i++) {
+//     topics[i].onclick = (e) => {
+//         let topicDropdown = topics[i].lastElementChild;
+//         if(topicDropdown){
+//             if(topicDropdown.classList.contains('hidden')){
+//                 topicDropdown.classList.remove('hidden');
+//                 alert("hidden removed")
+//             }
+//             else
+            
+//         }
+//     }
+// }
